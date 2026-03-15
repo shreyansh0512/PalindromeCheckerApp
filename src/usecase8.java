436973fd162a26e390d7f8295f5c846f67f39a17
shreyansh0512 <shreyanshtiwari785@gmail.com>
2026-03-15 18:48:59 +0530

    Create usecase8.java
    UC8 — Linked List Based Palindrome Checker

---
+++ src/usecase8.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class UseCase8PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+
+            if (!list.removeFirst().equals(list.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
